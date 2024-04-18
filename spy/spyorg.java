5b064738d9453053f7dfc55aacc40d8f647a58f7
ADITHYA19C <129170136+ADITHYA19C@users.noreply.github.com>
2024-04-18 23:23:20 +0530

    Update spyorg.java

--- spy/spyorg.java
+++ spy/spyorg.java
@@ -1,6 +1,15 @@
+<dependencies>
+  <dependency>
+  <groupId>junit</groupId>
+  <artifactId>junit</artifactId>
+  <version>3.9.6</version>
+  <scope>test</scope>
+  
 hello team-
 everyone start
 to deploy the code
 once deployed
 inform here
 done
+  </dependency>
+  </dependencies>
